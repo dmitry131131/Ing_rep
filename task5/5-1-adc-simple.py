06dd1441d65758aa2dffa9e007e825ca5ec2ab25
dmitry <antontsev.dmitry@yandex.ru>
2024-04-12 17:13:18 +0300

    Made task 5

---
+++ task5/5-1-adc-simple.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+dac    = [8, 11, 7, 1, 0, 5, 12, 6]
+troyka = 13
+comp   = 14
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+
+GPIO.setup(comp, GPIO.IN)
+
+
+def dbl(number):
+    binary_list = []
+    while number > 0:
+        remainder = number % 2
+        binary_list.insert(0, remainder)
+        number = number // 2
+    while len(binary_list) < 8:
+        binary_list.insert(0, 0)
+    return binary_list
+
+
+def adc():
+    for i in range(256):
+        GPIO.output(dac, dbl(i))
+        sleep(0.01)
+        tmp = GPIO.input(comp)
+        if tmp:
+            return i
+    return 0
+
+
+try:
+    while True:
+        i = 3.3 * adc() / 256.0
+        if i:
+            print("current volt: ", i)
+finally:
+    GPIO.output(dac, 0)
+    GPIO.output(troyka, 0)
+    GPIO.cleanup()
+    print("EOP")
---
+++ task5/5-3-adc.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+dac    = [8, 11, 7, 1, 0, 5, 12, 6]
+led    = [2, 3, 4, 17, 27, 22, 10, 9]
+troyka = 13
+comp   = 14
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+
+GPIO.setup(comp, GPIO.IN)
+
+
+def dbl(number):
+    binary_list = []
+    while number > 0:
+        remainder = number % 2
+        binary_list.insert(0, remainder)
+        number = number // 2
+    while len(binary_list) < 8:
+        binary_list.insert(0, 0)
+    return binary_list
+
+
+def adc():
+    res = 0
+    i = 128
+    while i >= 1:
+        GPIO.output(dac, dbl(int(i + res)))
+        sleep(0.01)
+        tmp = GPIO.input(comp)
+        if tmp == 0:
+            res += i
+        i /= 2
+    
+    return res
+
+
+try:
+    while True:
+        q = adc()
+        i = 3.3 * q / 256.0
+        print("Current volt: ", i)
+        tmp = dbl(2**int(q  / 32) - 1)
+        GPIO.output(led, tmp)
+finally:
+    GPIO.output(dac, 0)
+    GPIO.output(led, 0)
+    GPIO.output(troyka, 0)
+    GPIO.cleanup()
+    print("EOooo")
